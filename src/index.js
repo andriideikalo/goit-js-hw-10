83b4c5c56b8efc59387cfa0086e2b2c5e1b95cfc
andriideikalo <wlada801@gmail.com>
2022-11-25 21:49:46 +0200

    add notiflix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+Notiflix.Notify.warning('Memento te hominem esse');
+
+Notiflix.Notify.info('Cogito ergo sum');
+
+
 
 const DEBOUNCE_DELAY = 300;
